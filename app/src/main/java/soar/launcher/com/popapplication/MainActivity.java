24352ae1d8c7120957f3658a4eb512cd108d4785
gaofei <gaofei@yonglibao.com>
2016-11-11 15:20:31 +0800

    add project

---
+++ app/src/main/java/soar/launcher/com/popapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package soar.launcher.com.popapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.test);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GeneralAccessDialog generalAccessDialog = new GeneralAccessDialog.Builder(MainActivity.this)
+                        .setTitle("gaofei")
+                        .setTitleImage(R.drawable.test)
+                        .setDialogBackColor(Color.RED)
+                        .setDialogOutBackColor(Color.parseColor("#5500ff00"))
+                        .setMessage("this is test")
+                        .setNegativeButton("test-na", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).setPositiveButton("test-ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).setDialogDismissLisenter(new GeneralAccessDialog.DialogDismissLisenter() {
+                            @Override
+                            public void onDismiss() {
+
+                            }
+                        }).build()
+                        .show();
+
+
+            }
+        });
+
+    }
+}
